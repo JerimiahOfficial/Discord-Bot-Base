e0c84801094d061d860d2c6b50a24c9985cb2499
Jerimiah <38990407+JerimiahOfficial@users.noreply.github.com>
2021-08-07 00:47:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const { Client, Collection } = require('discord.js');
 const bot = new Client();
 const { token } = require(`./botconfig.json`);
-const { prefix } = require(`./config.json`);
 
 [`aliases`, `commands`].forEach(x => bot[x] = new Collection());
 ["command", "events"].forEach(x => require(`./handlers/${x}`)(bot));
