9651c6e19f02827d30b5bcd75b3ab04e5d0e48ac
Jerimiah <38990407+JerimiahOfficial@users.noreply.github.com>
2021-08-07 00:48:53 -0400

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -2,5 +2,5 @@ const Discord = require('discord.js');
 const { version } = require('./../../botconfig.json')
 
 module.exports = async (bot) => {
-    console.log(`Bot has initialized.`)
+    console.log(`Bot ${version} has initialized.`)
 }
