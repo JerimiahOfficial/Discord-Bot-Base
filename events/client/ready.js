42e8ba1df722e7543e275619f37b3e17023c12bc
Jerimiah <38990407+JerimiahOfficial@users.noreply.github.com>
2021-08-19 18:50:25 -0400

    Delete events/client directory

--- events/client/ready.js
+++
@@ -1,6 +0,0 @@
-const Discord = require('discord.js');
-const { version } = require('./../../botconfig.json')
-
-module.exports = async (bot) => {
-    console.log(`Bot ${version} has initialized.`)
-}
