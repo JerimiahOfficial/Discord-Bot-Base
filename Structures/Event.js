d2394e54ade58e33523a5db2c2b9db9acc3bcd4f
Jerimiah <38990407+JerimiahOfficial@users.noreply.github.com>
2021-08-19 15:49:43 -0700

    Add files via upload

---
+++ Structures/Event.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+const Client = require("./Client.js");
+
+/**
+ * @template {keyof Discord.ClientEvents} K
+ * @param {Client} client 
+ * @param  {DiscordAPIError.ClientEvents[K]} eventArgs 
+ */
+function Run(client, ...eventArgs) {}
+
+/**
+ * @template {keyof Discord.ClientEvents} K
+ */
+class Event {
+    /**
+     * 
+     * @param {K} event 
+     * @param {Run<K>} run 
+     */
+    constructor(event, run) {
+        this.event = event;
+        this.run = run;
+    }
+}
+module.exports = Event;
