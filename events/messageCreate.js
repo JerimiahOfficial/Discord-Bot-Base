b998585b60718ce98dd464a334cdee75633aeb00
Jerimiah <38990407+JerimiahOfficial@users.noreply.github.com>
2022-08-13 21:36:15 -0400

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,46 +1,21 @@
 const Event = require("../Structures/Event.js");
 
 module.exports = new Event("messageCreate", (client, message) => {
-    if (message.author.bot) return;
+    if (message.channel.type === 1)
+        return;
 
-    // Find emoji's by id
-    const yes = client.emojis.cache.get("870100889837371463");
-    const no = client.emojis.cache.get("870100889480888351");
+    if (message.author.bot) 
+        return;
 
-    switch (message.channelId) {
-        case "870098799274627133": // Memes
-                if (message.attachments.some(attachment => {
-                    // Get the extension from the attachment name
-                    const parts = attachment.name.split('.')
-                    const extension = parts[parts.length - 1]
+    if (!message.content.startsWith("!")) 
+        return;
 
-                    // Check if the extension is one of these ones
-                    return ['png', 'jpg', 'jpeg', 'gif', 'mp4', 'webm', 'mov'].includes(extension)
-                })) {
-                    // Reacted with emoji's
-                    message.react(yes);
-                    message.react(no);
-                }
-                break;
-        case "870105844455440395":
-            // Delete senders original message.
-            setTimeout(() => message.delete(), 250);
-            break;
-        case "870105870556606535":
-            // Delete senders original message.
-            setTimeout(() => message.delete(), 250);
-            break;
-        default:
-            break;
-    }
+    const content = message.content.split(" ");
+    const command = client.commands.find((cmd) => cmd.name == content[0].substr(1).toLowerCase());
+    const arguments = content.slice(1);
 
-	if (!message.content.startsWith(client.prefix)) return;
+    if (!command)
+	return;
 
-	const args = message.content.substring(client.prefix.length).split(/ +/);
-
-	const command = client.commands.find(cmd => cmd.name == args[0]);
-
-	if (!command) return message.reply(`${args[0]} is not a valid command!`);
-
-	command.run(message, args, client);
+    command.run(message, arguments, client);
 });
