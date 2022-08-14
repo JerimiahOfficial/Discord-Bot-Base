c4f14a8feaa52e42a1c95da1a339e96f9162a873
Jerimiah <38990407+JerimiahOfficial@users.noreply.github.com>
2022-08-13 21:25:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
-const Client = require('./Structures/Client.js');
-const Config = require('./botconfig.json');
-const client = new Client();
-
-client.start(Config.token);
+const Client = require("./Structures/Client.js");
+new Client().start('<bot token here>');
