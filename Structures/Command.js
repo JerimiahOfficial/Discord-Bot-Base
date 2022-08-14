51bbab4f289e2799e52b435d26576b33e5bebdb3
Jerimiah <38990407+JerimiahOfficial@users.noreply.github.com>
2022-08-13 21:26:59 -0400

    Update Command.js

--- Structures/Command.js
+++ Structures/Command.js
@@ -1,21 +1,20 @@
-const Discord = require('discord.js');
-const Client = require('./Client.js');
+const Discord = require("discord.js");
+const Client = require("./Client.js");
 
 /**
- * @param {Discord.Message | Discord.Interaction} message 
- * @param {String[]} args 
- * @param {Client} client 
+ * @param {Discord.Message | Discord.Interaction} message
+ * @param {String[]} args
+ * @param {Client} client
  */
 function Run(message, args, client) {}
 
 class Command {
     /**
-     * @typedef {{name: string, description: string, run: Run}} CommandOptions
-     * @param {CommandOptions} options 
+     * @typedef {{name: string, run: Run}} CommandOptions
+     * @param {CommandOptions} options
      */
     constructor(options) {
         this.name = options.name;
-        this.description = options.description;
         this.run = options.run;
     }
 }
