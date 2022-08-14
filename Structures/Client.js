694923986830fd81a117b1f14111eff0d0a23170
Jerimiah <38990407+JerimiahOfficial@users.noreply.github.com>
2022-08-13 21:28:44 -0400

    Update Client.js

--- Structures/Client.js
+++ Structures/Client.js
@@ -1,41 +1,41 @@
-const fs = require('fs');
+const Command = require("./Command.js");
 const Discord = require("discord.js");
-const Command = require('./Command.js');
-const Event = require('./Event.js');
-const Config = require('../botconfig.json');
-const intents = new Discord.Intents(32767);
+const Event = require("./Event.js");
+const fs = require("fs");
+const Logger = require("../helpers/logger.js");
 
 class Client extends Discord.Client {
     constructor() {
-        super( { intents })
+        super({ intents: [ <itents that your bot is gonna use here> ] });
         /**
          * @type {Discord.Collection<string, Command>}
          */
         this.commands = new Discord.Collection();
-        this.prefix = Config.prefix;
+        this.prefix = '<prefix that your bot is gonna use>';
     }
 
     start(token) {
-        console.log(`${Config.consoleprefix}Initializing commands.`);
-        fs.readdirSync("./commands/").filter(file => file.endsWith(".js")).forEach(file => {
-            /**
-             * @type {Command}
-             */
-            const command = require(`../commands/${file}`);
-            console.log(`\t${command.name} loaded.`);
-            this.commands.set(command.name, command)
-        })
+        fs.readdirSync("./commands/")
+            .filter((file) => file.endsWith(".js"))
+            .forEach((file) => {
+                /**
+                 * @type {Command}
+                 */
+                const command = require(`../commands/${file}`);
+                this.commands.set(command.name, command);
+            });
 
-        console.log(`${Config.consoleprefix}Initializing events.`);
-        fs.readdirSync('./events/').filter(file => file.endsWith(".js")).forEach(file => {
-            /**
-             * @type {Event}
-             */
-            const event = require(`../events/${file}`);
-            console.log(`\t${event.event} loaded.`);
-            this.on(event.event, event.run.bind(null, this));
-        })
-        this.login(token)
+        fs.readdirSync("./events/")
+            .filter((file) => file.endsWith(".js"))
+            .forEach((file) => {
+                /**
+                 * @type {Event}
+                 */
+                const event = require(`../events/${file}`);
+                this.on(event.event, event.run.bind(null, this));
+            });
+            
+        this.login(token);
     }
 }
 module.exports = Client;
