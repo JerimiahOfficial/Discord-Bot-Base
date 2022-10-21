795a9d69468d025dbdd431acdf0ec81aca77c960
Gage Lodba <JerimiahOfficial@gmail.com>
2022-10-20 20:20:34 -0400

    Update index.ts

--- source/index.ts
+++ source/index.ts
@@ -1,3 +1,3 @@
 import Client from './structure/client'
 
-void new Client().start('ODAxNzk5MDE5MzcwMTE5MTg4.G1F4K6.fzKGK3G1KQoNonI__M7t3uRY7BX3HlJN7ntcuM')
+void new Client().start('<bot token here>')
