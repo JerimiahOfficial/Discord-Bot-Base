319564269426a374d48a0733a93a1e0999ef028a
Gage Lodba <38990407+JerimiahOfficial@users.noreply.github.com>
2023-01-20 20:57:48 -0500

    Fixed misused promises
    Changed for each loops to maps in return resolving the promises.

--- source/structure/client.ts
+++ source/structure/client.ts
@@ -1,6 +1,5 @@
-/* eslint-disable @typescript-eslint/no-misused-promises */
 import { Collection, Client } from 'discord.js'
-import * as fs from 'fs'
+import { readdirSync } from 'node:fs'
 import Logger from '../helpers/logger'
 import Command from './command'
 import Event from './event'
@@ -8,46 +7,57 @@ import Path from 'path'
 
 export default class client extends Client {
   commands: Collection<string, Command> = new Collection()
+  slashs: Collection<string, Slash> = new Collection()
 
   constructor () {
     super({
       intents: [
-        'MessageContent', 'Guilds', 'GuildScheduledEvents', 'GuildPresences', 'GuildMessages', 'GuildMessageReactions',
-        'GuildMembers', 'GuildIntegrations', 'GuildBans', 'DirectMessages', 'DirectMessageReactions'
+        'MessageContent', 'Guilds', 'GuildScheduledEvents', 'GuildPresences', 'GuildMessages',
+        'GuildMessageReactions', 'GuildMembers', 'GuildIntegrations', 'GuildBans', 'DirectMessages',
+        'DirectMessageReactions'
       ]
     })
   }
 
-  async start (token: string): Promise<void> {
-    const commands = fs.readdirSync('./dist/commands/')
-    commands.forEach(async (file) => {
+  async importCommands (): Promise<void> {
+    const commands = readdirSync('./dist/commands/')
+
+    commands.map(async (file) => {
       await import(Path.join(__dirname, '../commands/', file)).then((file) => {
         const command = file.default as Command | undefined
 
         if (command === undefined) return
 
         this.commands.set(command.name, command)
-        Logger('cyan', `\t: ${command.name} command loaded.`, true)
+        Logger('cyan', `[+] ${command.name} command loaded.`, true)
       }).catch((err) => {
-        Logger('red', `\t: ${file} couldn't be loaded.`, true)
+        Logger('red', `[-] ${file} couldn't be loaded.`, true)
         console.error(err)
       })
     })
+  }
 
-    const events = fs.readdirSync('./dist/events/')
-    events.forEach(async (file) => {
+  async importEvents (): Promise<void> {
+    const events = readdirSync('./dist/events/')
+
+    events.map(async (file) => {
       await import(Path.join(__dirname, '../events/', file)).then((file) => {
         const event = file.default as Event | undefined
 
         if (event === undefined) return
 
         this.on(event.event, event.run.bind(null, this))
-        Logger('cyan', `\t: ${event.event} event loaded.`, true)
+        Logger('cyan', `[+] ${event.event} event loaded.`, true)
       }).catch((err) => {
-        Logger('red', `\t: ${file} couldn't be loaded.`, true)
+        Logger('red', `[-] ${file} couldn't be loaded.`, true)
         console.error(err)
       })
     })
+  }
+
+  async start (token: string): Promise<void> {
+    await this.importCommands()
+    await this.importEvents()
 
     await this.login(token)
   }
