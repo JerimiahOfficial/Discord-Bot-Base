29f365849bba2e59a544afaad602e1d3ff34940f
Jerimiah <38990407+JerimiahOfficial@users.noreply.github.com>
2024-01-19 17:48:46 -0500

    Small changes

--- source/helpers/Loader.ts
+++ source/helpers/Loader.ts
@@ -7,16 +7,18 @@ export default async function Load<T> (directory: string, register: (o: T) => vo
   Logger('cyan', `\nLoading ${directory}...`)
   const files = readdirSync(`./dist/${directory}/`)
 
-  for (const file of files) {
-    try {
-      const { default: object } = await import(join(__dirname, '..', directory, file)) as { default: T | undefined }
+  await Promise.all(
+    files.map(async file => {
+      try {
+        const { default: object } = await import(join(__dirname, '..', directory, file)) as { default: T | undefined }
 
-      if (object === undefined) return
+        if (object === undefined) return
 
-      register(object)
-      Logger('cyan', `[+] ${file} loaded.`)
-    } catch {
-      Logger('red', `[-] ${file} failed to load.`)
-    }
-  }
+        register(object)
+        Logger('green', `[+] ${file} loaded.`)
+      } catch {
+        Logger('red', `[-] ${file} failed to load.`)
+      }
+    })
+  )
 }
--- source/helpers/Register.ts
+++ source/helpers/Register.ts
@@ -11,23 +11,20 @@ if (process.env.TOKEN === undefined) throw new Error('TOKEN is not defined.')
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN)
 
-void (async () => {
+async function RegisterSlashes (): Promise<void> {
   const slashes: any[] = []
   await Load<Slash>('slashes', slash => slashes.push(slash.data.toJSON()))
 
-  try {
-    Logger('cyan', 'Started refreshing application (/) commands.')
+  Logger('yellow', 'Started refreshing application (/) commands.')
 
-    if (process.env.CLIENT_ID === undefined) throw new Error('CLIENT_ID is not defined.')
+  if (process.env.CLIENT_ID === undefined) throw new Error('CLIENT_ID is not defined.')
 
-    await rest.put(
-      Routes.applicationCommands(process.env.CLIENT_ID),
-      { body: slashes }
-    )
+  await rest.put(
+    Routes.applicationCommands(process.env.CLIENT_ID),
+    { body: slashes }
+  )
+}
 
-    Logger('cyan', 'Successfully reloaded application (/) commands.')
-  } catch (error) {
-    console.error(error)
-    Logger('red', 'Couldn\'t reload application (/) commands.')
-  }
-})()
+RegisterSlashes()
+  .then(() => { Logger('green', 'Registered Slashes.') })
+  .catch(() => { Logger('red', 'Failed to register Slashes.') })
