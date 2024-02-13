35c724aedf238f9d15abfed4a257f71d540b0080
Jerimiah <38990407+JerimiahOfficial@users.noreply.github.com>
2024-02-13 00:41:07 -0500

    Small changes

--- source/commands/info.ts
+++ source/commands/info.ts
@@ -1,40 +1,40 @@
 import {
-  type ActionRowData,
   ButtonStyle,
-  ComponentType,
-  type MessageActionRowComponentBuilder,
-  type MessageActionRowComponentData
+  ComponentType
 } from 'discord.js'
 
 import Command from '../structure/Command'
-
-const row: ActionRowData<MessageActionRowComponentData | MessageActionRowComponentBuilder> = {
-  type: ComponentType.ActionRow,
-  components: [
-    {
-      type: ComponentType.Button,
-      label: 'Github',
-      style: ButtonStyle.Link,
-      url: 'https://github.com/JerimiahOfficial'
-    }
-  ]
-}
 
 export default new Command(
   'info',
   async (client, message, _args) => {
     if (client.user == null) return
 
-    const infoEmbed = {
-      author: {
-        name: client.user.username,
-        iconURL: client.user.displayAvatarURL()
-      },
-      color: 0xffff00,
-      description: '**Bot base creator:** <@196494542768177154>'
-    }
-
-    await message.channel.send({ embeds: [infoEmbed], components: [row] })
+    await message.channel.send(
+      {
+        embeds: [{
+          author: {
+            name: client.user.username,
+            icon_url: client.user.displayAvatarURL()
+          },
+          color: 0xffff00,
+          description: '**Bot base creator:** <@196494542768177154>'
+        }],
+        components: [
+          {
+            type: ComponentType.ActionRow,
+            components: [
+              {
+                type: ComponentType.Button,
+                label: 'Github',
+                style: ButtonStyle.Link,
+                url: 'https://github.com/JerimiahOfficial'
+              }
+            ]
+          }
+        ]
+      }
+    )
     await message.delete()
   }
 )
